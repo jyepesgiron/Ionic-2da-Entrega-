944edb3853530fd1b3b01f55a6cb8d256993b146
jyepesgiron <126540597+jyepesgiron@users.noreply.github.com>
2023-11-14 16:56:48 -0500

    modificacion.component.ts

--- src/app/Sesion/admin/modificacion/modificacion.component.ts
+++ src/app/Sesion/admin/modificacion/modificacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modificacion',
@@ -7,8 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModificacionComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private alerta: AlertController) { }
 
   ngOnInit() {}
+
+  async op(){
+    const alert = await this.alerta.create({
+      header: 'Rango',
+      message: 'Seleccione el estado del AnteProyecto',
+      buttons: [
+        {
+          text: 'Pendiente',
+          handler: () => {
+            console.log('Opción 1 seleccionada');
+            // Realiza acciones específicas para la opción 1 aquí
+          }
+        },
+        {
+          text: 'Revisado',
+          handler: () => {
+            console.log('Opción 2 seleccionada');
+            // Realiza acciones específicas para la opción 2 aquí
+          }
+        },
+        {
+          text: 'Aprovado',
+          handler: () => {
+            console.log('Opción 3 seleccionada');
+            // Realiza acciones específicas para la opción 2 aquí
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelado');
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
 
 }
